1eac91a01c5e3399ac62b2556ef3e841f4d9ce54
Matheus <m.eiki.ikeda@gmail.com>
2024-10-03 11:24:11 -0300

    Criando função em javascript

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 //ESTRUTURA DE REPETIÇÃO
 for (let i=0; i<10; i++){
-    console.log(i)
+    //console.log(i)
 }
 //WHILE
 
 let f = 0;
 while (f<10){
-    console.log(f)
+    //console.log(f)
     f++;
 }
+
+//Do while
+let w= 0;
+
+do{
+    //console.log(w)
+    w++;
+}
+while(w<5);
+
+//funções
+//`` (strings literal ou template string ${} chama a variável)
+function Ola(nome){
+    //console.log(`Seja bem vindo, a ${nome}`)
+    alert(`seja bem vindo a ${nome}`)
+    document.write(`seja bem vindo a ${nome}`)
+}
+//executando a fun~]ao Ola passando o valor
+Ola("FIAP")
+
